aeb5d529378b1da4279fc8ebc3b41e1aeb6fe368
Metbcy <Amirbredy1@gmail.com>
2023-02-04 18:14:38 -0500

    Test of assignment

--- lsps2023/src/org/howard/edu/assignment2/Hw2.java
+++ lsps2023/src/org/howard/edu/assignment2/Hw2.java
@@ -1,5 +1,47 @@
 package org.howard.edu.assignment2;
 
-public class Hw2 {
+import java.io.InputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.net.URL;
+import java.util.HashMap;
 
+public class Hw2 {
+	public String readToString(String resource) throws FileNotFoundException {
+        HashMap<String, Integer> wordFrequency = new HashMap<>();
+        URL url = getClass().getClassLoader().getResource(resource);
+        if (url != null) {
+			File file = new File(url.getPath());
+		Scanner scanner = null;
+        try {
+            while (scanner.hasNextLine()) {
+                String[] words = scanner.nextLine().split(" ");
+                for (String word : words) {
+                    word = word.replace(".", "");
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    if (word.length() <= 3) {
+                        continue;
+                    }
+                    
+                    if (!word.matches("[0-9]+")) {
+                        if (wordFrequency.containsKey(word)) {
+                            wordFrequency.put(word, wordFrequency.get(word) + 1);
+                        } else {
+                            wordFrequency.put(word, 1);
+                        }
+                    }
+                }
+            }
+            scanner.close();
+            for (String word : wordFrequency.keySet()) {
+                System.out.println(word + ": " + wordFrequency.get(word));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: Unable to find textfile.txt");
+        }
+    }
 }

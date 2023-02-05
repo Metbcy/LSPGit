f4ffaafd08d9e410ec1ab4b6ced1273bbb566707
Metbcy <Amirbredy1@gmail.com>
2023-02-04 21:19:43 -0500

    testing

--- lsps2023/src/org/howard/edu/assignment2/Hw2.java
+++ lsps2023/src/org/howard/edu/assignment2/Hw2.java
@@ -1,6 +1,5 @@
 package org.howard.edu.assignment2;
 
-import java.io.InputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -9,40 +8,41 @@ import java.util.HashMap;
 
 public class Hw2 {
 	public String readToString(String resource) throws FileNotFoundException {
-        HashMap<String, Integer> wordFrequency = new HashMap<>();
         URL url = getClass().getClassLoader().getResource(resource);
         if (url != null) {
 			File file = new File(url.getPath());
-		Scanner scanner = null;
-        try {
-            while (scanner.hasNextLine()) {
-                String[] words = scanner.nextLine().split(" ");
-                for (String word : words) {
-                    word = word.replace(".", "");
-                    if (word.isEmpty()) {
-                        continue;
-                    }
-                    word = word.toLowerCase();
-                    if (word.length() <= 3) {
-                        continue;
-                    }
-                    
-                    if (!word.matches("[0-9]+")) {
-                        if (wordFrequency.containsKey(word)) {
-                            wordFrequency.put(word, wordFrequency.get(word) + 1);
-                        } else {
-                            wordFrequency.put(word, 1);
-                        }
-                    }
-                }
-            }
-            for (String word : wordFrequency.keySet()) {
-                System.out.println(word + ": " + wordFrequency.get(word));
-            }
-        } finally {
-        	scanner.close();
-        }
-    }
-        throw new FileNotFoundException();
-}
-}
+			Scanner scanner = null;
+	        try {
+	        	scanner = new Scanner(file);
+				HashMap<String, Integer> wordFrequency = new HashMap<>();
+	            while (scanner.hasNextLine()) {
+	                String[] words = scanner.nextLine().split(" ");
+	                for (String word : words) {
+	                    word = word.replace(".", "");
+	                    if (word.isEmpty()) {
+	                        continue;
+	                    }
+	                    word = word.toLowerCase();
+	                    if (word.length() <= 3) {
+	                        continue;
+	                    }
+	                    
+	                    if (!word.matches("[0-9]+")) {
+	                        if (wordFrequency.containsKey(word)) {
+	                            wordFrequency.put(word, wordFrequency.get(word) + 1);
+	                        } else {
+	                            wordFrequency.put(word, 1);
+	                        }
+	                    }
+	                }
+	            }
+	            
+	        } finally {
+	        	scanner.close();
+	        }
+	    }
+	        throw new FileNotFoundException();
+	}
+	}
+
+

e9400cd287d19baa1e3318e7981b4b2c24db9187
Metbcy <Amirbredy1@gmail.com>
2023-03-02 10:17:08 -0500

    Adding more print statements/tests

--- lsps2023/src/org/howard/edu/assignment4/Driver.java
+++ lsps2023/src/org/howard/edu/assignment4/Driver.java
@@ -18,39 +18,50 @@ public class Driver {
 		// Function testing for our First Set
 		FirstSet.add(62);
 		FirstSet.add(93);
+		//Test prints in between to show that the addition is working
+		System.out.println("The current value of our FirstSet is: \n" + FirstSet.toString());
 		FirstSet.add(15);
 		FirstSet.add(23);
+		System.out.println("The current value of our FirstSet is: \n" + FirstSet.toString());
 		FirstSet.add(35);
 		
 		// Function testing for our Second set
 		SecondSet.add(2);
 		SecondSet.add(3);
+		System.out.println("The current value of our SecondSet is: \n" + SecondSet.toString());
+		SecondSet.add(30);
+		SecondSet.add(70);
+		System.out.println("The current value of our SecondSet is: \n" + SecondSet.toString());
+		SecondSet.add(35);
 		
 		// Function testing for our Third set
 		ThirdSet.add(60);
 		ThirdSet.add(100);
 		ThirdSet.add(350);
+		System.out.println("The current value of our ThirdSet is: \n" + ThirdSet.toString());
 		ThirdSet.add(600);
 		
 		//Print statements
 		System.out.println("The value of FirstSet is: \n" + FirstSet.toString());
 		System.out.println("The value of SecondSet is: \n" + SecondSet.toString());
-		System.out.println("The value of setE is: \n" + EmptySet.toString());
+		System.out.println("The value of our EmptySet is: \n" + EmptySet.toString());
 		System.out.println("The value of ThirdSet is: \n" + ThirdSet.toString());
 
 		// Function 1 test
 		System.out.println("Does the First Set contains the number 1?: \n" + FirstSet.contains(1));
+		System.out.println("Does the Second Set contains the number 3?: \n" + SecondSet.contains(3));
+		System.out.println("Does the Third Set contains the number 50?: \n" + ThirdSet.contains(50));
 		
 		//Testing length method with the sets
-		System.out.println("Our populated Set size is: \n" + FirstSet.length());
+		System.out.println("Our populated Set size is: \n" + SecondSet.length());
 		System.out.println("Our empty Set size is: \n" + EmptySet.length());
 		
 		// Testing smallest and largest methods with the sets
 		System.out.println("The smallest value in our First Set is: \n" + FirstSet.smallest());
-	
 		System.out.println("The smallest value in our Empty Set is: \n" + EmptySet.smallest());
 		System.out.println("The largest value in our First set is: \n" + FirstSet.largest());
-		System.out.println("The largest value in our First set is: \n" + EmptySet.largest());
+		System.out.println("The largest value in our Empty set is: \n" + EmptySet.largest());
+		System.out.println("The largest value in our Third set is: \n" + ThirdSet.largest());
 		
 		// Testing the equals method with the sets
 		System.out.println("Does the First set equal the Second set? \n" + FirstSet.equals(SecondSet));

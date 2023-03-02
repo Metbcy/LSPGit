1e639c0a7d8370df256acab343b8f1c7374d46a1
Metbcy <Amirbredy1@gmail.com>
2023-03-01 23:27:56 -0500

    Integer Set and Driver code drafts

--- lsps2023/src/org/howard/edu/assignment4/Driver.java
+++ lsps2023/src/org/howard/edu/assignment4/Driver.java
@@ -1,7 +1,80 @@
 package org.howard.edu.assignment4;
 
+/**
+ * 
+ * @author Amir Bredy
+ *
+ */
 
 public class Driver {
 	public static void main(String[] args) {
-	}
-	}
+		//Testing set, Will throw exception
+		IntegerSet EmptySet = new IntegerSet();
+		//Initializing our three sets
+		IntegerSet FirstSet = new IntegerSet();
+		IntegerSet SecondSet = new IntegerSet();
+		IntegerSet ThirdSet = new IntegerSet();
+		
+		// Function testing for our First Set
+		FirstSet.add(62);
+		FirstSet.add(93);
+		FirstSet.add(15);
+		FirstSet.add(23);
+		FirstSet.add(35);
+		
+		// Function testing for our Second set
+		SecondSet.add(2);
+		SecondSet.add(3);
+		
+		// Function testing for our Third set
+		ThirdSet.add(60);
+		ThirdSet.add(100);
+		ThirdSet.add(350);
+		ThirdSet.add(600);
+		
+		//Print statements
+		System.out.println("The value of FirstSet is: \n" + FirstSet.toString());
+		System.out.println("The value of SecondSet is: \n" + SecondSet.toString());
+		System.out.println("The value of setE is: \n" + EmptySet.toString());
+		System.out.println("The value of ThirdSet is: \n" + ThirdSet.toString());
+
+		// Function 1 test
+		System.out.println("Does the First Set contains the number 1?: \n" + FirstSet.contains(1));
+		
+		//Testing length method with the sets
+		System.out.println("Our populated Set size is: \n" + FirstSet.length());
+		System.out.println("Our empty Set size is: \n" + EmptySet.length());
+		
+		// Testing smallest and largest methods with the sets
+		System.out.println("The smallest value in our First Set is: \n" + FirstSet.smallest());
+	
+		System.out.println("The smallest value in our Empty Set is: \n" + EmptySet.smallest());
+		System.out.println("The largest value in our First set is: \n" + FirstSet.largest());
+		System.out.println("The largest value in our First set is: \n" + EmptySet.largest());
+		
+		// Testing the equals method with the sets
+		System.out.println("Does the First set equal the Second set? \n" + FirstSet.equals(SecondSet));
+		System.out.println("Does the First set equal the Third set? \n" + ThirdSet.equals(FirstSet));
+		
+		// Testing the intersect method with the sets
+		System.out.println("First set before using intersect function: \n" + FirstSet.toString());
+		System.out.println("Second set before using intersect function: \n" + SecondSet.toString());
+		FirstSet.intersect(SecondSet);
+		System.out.println("First set after using intersect function: \n" + FirstSet.toString());
+		
+		//Testing isEmpty on the sets
+		System.out.println("Is our First Set empty? \n" + FirstSet.isEmpty());
+		System.out.println("Is our Empty set empty? \n" + EmptySet.isEmpty());
+		
+		// Test Union function on our sets
+		FirstSet.union(SecondSet);
+		System.out.println("Our first set after using Union is: \n" + FirstSet.toString());
+		
+		// Test diff function on the sets
+		FirstSet.diff(SecondSet);
+		System.out.println("The value of our first set after using diff with FirstSet and SecondSet is: \n" + FirstSet.toString());
+
+	};
+};
+
+	
--- lsps2023/src/org/howard/edu/assignment4/IntegerSet.java
+++ lsps2023/src/org/howard/edu/assignment4/IntegerSet.java
@@ -1,54 +1,147 @@
 package org.howard.edu.assignment4;
 
 import java.util.*; 
+/** 
+ * 
+ * @author Amir Bredy
+ *
+ */
 
-public class IntegerSet  {
-	// Hint: probably best to use an array list.  You will need to do a little research
-	private List<Integer> set = new ArrayList<Integer>();
-
-	// Default Constructor
-	public IntegerSet() {
-	}
-
-	// Clears the internal representation of the set
-public void clear() {…};
-
-// Returns the length of the set
-public int length() {…}; // returns the length
-
-/*
-              * Returns true if the 2 sets are equal, false otherwise;
- * Two sets are equal if they contain all of the same values in ANY order.
-*/
-public boolean equals(IntegerSet b) {…}; 
-
-// Returns true if the set contains the value, otherwise false
-public boolean contains(int value) {…};    
-
-// Returns the largest item in the set; Throws a IntegerSetException if the set is empty 
-public int largest()  {…}; 
-
-// Returns the smallest item in the set; Throws a IntegerSetException if the set is empty
-public int smallest()  IntegerSetException {…};
-
-	// Adds an item to the set or does nothing it already there	
- 	public void add(int item) {…}; // adds item to the set or does nothing if it is in set
-
-	// Removes an item from the set or does nothing if not there
-public void remove(int item) {…}; 
-
-// Set union
-public void union(IntegerSet intSetb) {…};
-
-// Set intersection
-public void intersect(IntegerSet intSetb) {…}; 
-
-// Set difference, i.e., s1 –s2
-public void diff(IntegerSet intSetb); // set difference, i.e. s1 - s2
-
-// Returns true if the set is empty, false otherwise
-boolean isEmpty(); 
-
-// Return String representation of your set
-public String toString() {…};	// return String representation of your set
+public class IntegerSet {
+	// Creating an Exception class
+    private class IntegerSetException extends Exception {
+        IntegerSetException(String str){
+            super(str);
+        }
+    }
+    
+    private ArrayList<Integer> set = new ArrayList<>();
+    
+    public IntegerSet() {}
+    
+    // Clearing the set
+    public void clear() {
+        set.clear();
+    }
+    // function to return the length of a set
+    public int length(){
+        return set.size();
+    }
+    // Boolean func that returns true if two sets are equal
+    public boolean equals(IntegerSet other) {
+        if (set.size() != other.set.size()) {
+            return false;
+        }
+        //for loop to compare sets
+        for (int i = 0; i < other.set.size(); i++) {
+            if (!set.contains(other.set.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    // Boolean func that returns true if a set contains a value, will take an integer.
+    public boolean contains(int value) {
+        return set.contains(value);
+    }
+    
+	// Function that returns the largest integer in the set
+	public int largest()  {
+		int max = 0;
+		try {
+			for (int i = 0; i< set.size(); i++) {
+				if (set.get(i) > max) {
+					max = set.get(i);
+				}
+			}
+			if (set.size() > 0) {
+				return max;
+			}
+			else {
+		    IntegerSetException emptySet = new IntegerSetException("This set is empty, we can't return the largest element");
+		    throw emptySet;
+			}
+		} 
+		catch (IntegerSetException emptySet){  
+			emptySet.printStackTrace();
+		}
+		
+		return 10000;
+	}; 
+	
+	// Function that returns the smallest integer in the set
+	public int smallest() {
+		int min = 0;
+		try {
+			for (int i = 0; i< set.size(); i++) {
+				if (set.get(i) < min) {
+					min = set.get(i);
+				}
+			}
+			if (set.size() > 0) {
+				return min;
+			}
+			else {
+		    IntegerSetException emptySet = new IntegerSetException("This set is empty, we can't return the smallest element");
+		    throw emptySet;
+			}
+		} 
+		catch (IntegerSetException emptySet){  
+			emptySet.printStackTrace();
+		}
+		return 0;
+	};
+	
+	// Function to add an integer to the set
+    public void add(int item) {
+        if (!set.contains(item)) {
+            set.add(item);
+        }
+    }
+    
+    // Function to remove an integer to the set
+    public void remove(int item) {
+        if (set.contains(item)){
+            set.remove(Integer.valueOf(item));
+        }
+    }
+    // Function to find the union of the current set and another set
+    public void union(IntegerSet other) {
+        assert set.size() > 0 && other.set.size() > 0;
+        ArrayList<Integer> temp = new ArrayList<>(set);
+        for (int i = 0; i < other.set.size(); i++) {
+            if (!temp.contains(other.set.get(i))) {
+                temp.add(other.set.get(i));
+            }
+        }
+        set = temp;
+    }
+    // Function to find the intersection of the current set and another set
+    public void intersect(IntegerSet other) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < set.size(); i++) {
+            if (other.set.contains(set.get(i))) {
+                temp.add(set.get(i));
+            }
+        }
+        set = temp;
+    }
+    // Function to find the difference of the current set and another set
+    public void diff(IntegerSet other) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < set.size(); i++) {
+            if (!other.set.contains(set.get(i))) {
+                temp.add(set.get(i));
+            }
+        }
+        set = temp;
+    }
+    // Function to find whether a set is empty
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+    // Returns the string representation of the IntegerSet
+    public String toString() {
+        return set.toString();
+    }
 }

aadee94950224a7773d7f464fa303e55268dbfb6
Metbcy <Amirbredy1@gmail.com>
2023-03-09 19:35:41 -0500

    Test exceptions

--- lsps2023/src/org/howard/edu/assignment5/IntegerSetTest.java
+++ lsps2023/src/org/howard/edu/assignment5/IntegerSetTest.java
@@ -3,8 +3,25 @@ package org.howard.edu.assignment5;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 class IntegerSetTest {
+	
+	IntegerSet set;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		set = new IntegerSet();
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+		set = null;
+	}
+
 
 	@Test
 	void test() {

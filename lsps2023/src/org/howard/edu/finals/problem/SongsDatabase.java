080307a63ac9053fc9a3ccf922be4516466d6c62
amirb <amirb@localhost>
2023-04-27 19:01:37 -0400

    Code Push Finals

--- lsps2023/src/org/howard/edu/finals/problem/SongsDatabase.java
+++ lsps2023/src/org/howard/edu/finals/problem/SongsDatabase.java
@@ -1,5 +1,42 @@
 package org.howard.edu.finals.problem;
 import java.util.HashMap;
-import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class SongsDatabase {
+    /* Key is the genre, HashSet contains associated songs */
+    private Map<String, HashSet<String>> map = new HashMap<String, HashSet<String>>();
+
+    /* Add a song title to a genre */
+    public void addSong(String genre, String songTitle) {
+        HashSet<String> songSet = map.get(genre);
+        if (songSet == null) {
+            songSet = new HashSet<String>();
+            map.put(genre, songSet);
+        }
+        songSet.add(songTitle);
+    }
+
+    /* Return the Set that contains all songs for a genre */
+    public Set<String> getSongs(String genre) {
+        HashSet<String> songSet = map.get(genre);
+        if (songSet == null) {
+            songSet = new HashSet<String>();
+        }
+        return songSet;
+    }
+
+    /* Return genre, i.e., jazz, given a song title */
+    public String getGenreOfSong(String songTitle) {
+        for (String genre : map.keySet()) {
+            HashSet<String> songSet = map.get(genre);
+            if (songSet.contains(songTitle)) {
+                return genre;
+            }
+        }
+        return null;
+    }
+}
+
 
--- lsps2023/src/org/howard/edu/finals/problem/SongsDatabaseTest.java
+++ lsps2023/src/org/howard/edu/finals/problem/SongsDatabaseTest.java
@@ -1,9 +1,68 @@
 package org.howard.edu.finals.problem;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import java.util.Set;
+import static org.junit.Assert.*;
 
-import org.howard.edu.midterm.problem51.Range.EmptyRangeException;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
+public class SongsDatabaseTest {
+    @Test
+    public void testSongsDatabase() {
+        SongsDatabase db = new SongsDatabase();
 
+        // Test adding and getting songs
+        db.addSong("Pop", "Love Yourself");
+        db.addSong("Pop", "Kill Bill");
+        db.addSong("Rock", "TNT");
+        db.addSong("Rock", "Hotel California");
+        db.addSong("Jazz", "What a Wonderful World");
+        db.addSong("Jazz", "Always There");
+        db.addSong("Rap", "Savage");
+        db.addSong("Rap", "Gin and Juice");
+        
+
+        Set<String> popSongs = db.getSongs("Pop");
+        assertTrue(popSongs.contains("Love Yourself"));
+        assertTrue(popSongs.contains("Kill Bill"));
+        assertFalse(popSongs.contains("Always There"));
+        assertFalse(popSongs.contains("TNT"));
+
+        Set<String> rockSongs = db.getSongs("Rock");
+        assertTrue(rockSongs.contains("TNT"));
+        assertTrue(rockSongs.contains("Hotel California"));
+        assertFalse(rockSongs.contains("Love Yourself"));
+        assertFalse(rockSongs.contains("Gin and Juice"));
+
+        Set<String> rapSongs = db.getSongs("Rap");
+        assertTrue(rapSongs.contains("Gin and Juice"));
+        assertTrue(rapSongs.contains("Savage"));
+        assertFalse(rapSongs.contains("Hotel California"));
+        assertFalse(rapSongs.contains("Kill Bill"));
+        
+        Set<String> jazzSongs = db.getSongs("Jazz");
+        assertTrue(jazzSongs.contains("Always There"));
+        assertTrue(jazzSongs.contains("What a Wonderful World"));
+        assertFalse(jazzSongs.contains("Kill Bill"));
+        assertFalse(jazzSongs.contains("TNT"));
+
+        // Test getting genre of song
+        assertEquals("Pop", db.getGenreOfSong("Kill Bill"));
+        assertEquals("Rock", db.getGenreOfSong("TNT"));
+        assertEquals("Rap", db.getGenreOfSong("Savage"));
+        assertNull(db.getGenreOfSong("Hello"));
+
+        // Test adding a song to a new genre
+        db.addSong("Reggae", "Girls Dem Sugar");
+        Set<String> countrySongs = db.getSongs("Reggae");
+        assertTrue(countrySongs.contains("Girls Dem Sugar"));
+        assertFalse(rapSongs.contains("Girls Dem Sugar"));
+
+        // Test adding a duplicate song to the same genre
+        db.addSong("Pop", "Kill Bill");
+        assertEquals(2, popSongs.size());
+        
+        //Print statements to check the code
+        System.out.println(db.getGenreOfSong("Savage")); // prints "Rap"
+        System.out.println(db.getGenreOfSong("Always There")); // prints "Jazz"
+        System.out.println(db.getGenreOfSong("Kill Bill")); // prints "Pop"
+        System.out.println(db.getGenreOfSong("Girls Dem Sugar")); // prints "Reggae"
+    }
+}
